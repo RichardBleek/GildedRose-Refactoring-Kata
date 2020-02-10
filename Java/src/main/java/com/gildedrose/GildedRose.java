4d5cee23cdd4a6eef751720b38b0ee979f9fadcf
Richard Bleek <richardbleek@gmail.com>
2020-02-10 20:41:42 +0100

    decrementQuality

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -13,7 +13,7 @@ class GildedRose {
                     && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (item.quality > 0) {
                     if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
+                        decrementQuality(item);
                     }
                 }
             } else {
@@ -39,7 +39,7 @@ class GildedRose {
                     if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (item.quality > 0) {
                             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality = item.quality - 1;
+                                decrementQuality(item);
                             }
                         }
                     } else {
@@ -50,6 +50,10 @@ class GildedRose {
                 }
             }
         }
+    }
+
+    private void decrementQuality(Item item) {
+        item.quality = item.quality - 1;
     }
 
     private void incrementQuality(Item item) {
